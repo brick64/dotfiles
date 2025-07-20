15864e8b315cb1d719eec7683369509457feb0c4
Nikita Mittal <nikita.mittal@yandex.ru>
2025-07-20 16:22:22 +0300

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+set -gx EDITOR nvim
+
+fzf --fish | source
+set -Ux FZF_DEFAULT_OPTS "--style full"
+set --universal nvm_default_version latest
+
+function fish_user_key_bindings
+  bind \ce 'ranger'
+end
+starship init fish | source
+
+zoxide init --cmd cd fish | source
